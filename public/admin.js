fd60a956fea625e025747abf39703cd080162ef1
Tuomas Kontola <tuomas.j.kontola@aalto.fi>
2019-04-09 13:14:01 +0300

    Adds gpd markers

--- public/admin.js
+++ public/admin.js
@@ -1,3 +1,21 @@
+let map;
+let markers;
+function getLocations() {
+  $.ajax({
+    type: "GET",
+    url: '/gps',
+    success: (response) => {
+      for(let guild of response) {
+        console.log(guild.latitude);
+        var lonLat = new OpenLayers.LonLat( guild.longitude, guild.latitude)
+          .transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject()
+        );
+        markers.addMarker(new OpenLayers.Marker(lonLat));
+        console.log(markers);
+      }
+    }
+  });
+}
 $(() => {
   $(".date").each( (index, obj) => {
     console.log($(obj).text());
@@ -5,7 +23,7 @@ $(() => {
     $(obj).text(d.toLocaleTimeString("it-IT"));
   });
 
-  const map = new OpenLayers.Map("mapdiv");
+  map = new OpenLayers.Map("mapdiv");
   map.addLayer(new OpenLayers.Layer.OSM());
   var lonLat = new OpenLayers.LonLat( 24.8275665, 60.190480099999995)
     .transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject()
@@ -14,9 +32,8 @@ $(() => {
   .transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject()
 );
   const zoom = 15;
-  const markers = new OpenLayers.Layer.Markers("Markers");
+  markers = new OpenLayers.Layer.Markers("Markers");
   map.addLayer(markers);
-  markers.addMarker(new OpenLayers.Marker(lonLat));
-  markers.addMarker(new OpenLayers.Marker(lonLat2));
-  map.setCenter(lonLat, zoom);
+  map.zoomToMaxExtent();
+  getLocations();
 });
