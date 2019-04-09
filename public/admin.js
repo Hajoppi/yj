3544a6053a8ecf595de4d30ec0fbf897f6ae63c1
Tuomas Kontola <tuomas.j.kontola@aalto.fi>
2019-04-09 12:59:23 +0300

    adds markers

--- public/admin.js
+++ public/admin.js
@@ -5,14 +5,18 @@ $(() => {
     $(obj).text(d.toLocaleTimeString("it-IT"));
   });
 
-  const map = new OpenLayers.Map("demoMap");
+  const map = new OpenLayers.Map("mapdiv");
   map.addLayer(new OpenLayers.Layer.OSM());
-  var lonLat = new OpenLayers.LonLat(60.190480099999995, 24.8275665)
+  var lonLat = new OpenLayers.LonLat( 24.8275665, 60.190480099999995)
     .transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject()
   );
-  const zoom = 16;
+  var lonLat2 = new OpenLayers.LonLat( 24.7275665, 60.190480099999995)
+  .transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject()
+);
+  const zoom = 15;
   const markers = new OpenLayers.Layer.Markers("Markers");
   map.addLayer(markers);
   markers.addMarker(new OpenLayers.Marker(lonLat));
+  markers.addMarker(new OpenLayers.Marker(lonLat2));
   map.setCenter(lonLat, zoom);
 });
